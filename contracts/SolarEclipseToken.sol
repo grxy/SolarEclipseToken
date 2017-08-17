7fe7711590e6d86d09264a72b565842dfc6d3330
Grex <punmasterjedi@gmail.com>
2017-08-17 15:37:47 -0600

    Mitigate short address attacks + race conditions

--- contracts/SolarEclipseToken.sol
+++ contracts/SolarEclipseToken.sol
@@ -12,7 +12,12 @@ contract Token {
 }
 
 contract StandardToken is Token {
-    function transfer(address _to, uint256 _value) returns (bool success) {
+    modifier onlyPayloadSize(uint numwords) {
+        assert(msg.data.length == numwords * 32 + 4);
+        _;
+    }
+
+    function transfer(address _to, uint256 _value) onlyPayloadSize(2) returns (bool success) {
         if (balances[msg.sender] >= _value && _value > 0) {
             balances[msg.sender] -= _value;
             balances[_to] += _value;
@@ -23,7 +28,7 @@ contract StandardToken is Token {
         }
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+    function transferFrom(address _from, address _to, uint256 _value) onlyPayloadSize(3) returns (bool success) {
         if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
             balances[_to] += _value;
             balances[_from] -= _value;
@@ -39,13 +44,15 @@ contract StandardToken is Token {
         return balances[_owner];
     }
 
-    function approve(address _spender, uint256 _value) returns (bool success) {
+    function approve(address _spender, uint256 _value) onlyPayloadSize(2) returns (bool success) {
+        require(_value == 0 || allowed[msg.sender][_spender] == 0);
+
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
         return true;
     }
 
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+    function allowance(address _owner, address _spender) constant onlyPayloadSize(2) returns (uint256 remaining) {
         return allowed[_owner][_spender];
     }
 
